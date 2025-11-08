c7bd0df87361b22ab8f7b25b253d18253d5bcb9d
MatiXxD <ilya_dinesenko@mail.ru>
2025-11-08 20:40:53 +0300

    fix: fix async

--- app/api.py
+++ app/api.py
@@ -1,30 +1,33 @@
-from fastapi import FastAPI, Depends
-from models.schemas import TextRequest, SentimentResponse, HealthResponse
+from fastapi import Depends, FastAPI
+
 from app.dependencies import get_sentiment_analyzer
 from core.sentiment import SentimentAnalyzer
+from models.schemas import HealthResponse, SentimentResponse, TextRequest
 
 app = FastAPI(
     title="Sentiment Analysis API",
     version="1.0.0",
-    description="API для анализа тональности русского текста"
+    description="API для анализа тональности русского текста",
 )
 
+
 @app.post("/analyze-sentiment", response_model=SentimentResponse)
-async def analyze_sentiment(
-    request: TextRequest,
-    analyzer: SentimentAnalyzer = Depends(get_sentiment_analyzer)
+def analyze_sentiment(
+    request: TextRequest, analyzer: SentimentAnalyzer = Depends(get_sentiment_analyzer)
 ):
     """
     Анализ тональности текста
-    
+
     - **context**: Текст для анализа тональности
     """
     sentiment_score = analyzer.get_sentiment(request.context)
     return SentimentResponse(sentiment=sentiment_score)
 
+
 @app.get("/", response_model=HealthResponse)
 async def root():
     return HealthResponse(status="active")
+
 
 @app.get("/health", response_model=HealthResponse)
 async def health_check():
